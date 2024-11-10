5cda965106ac76574551a9ff6c24882b5da79b46
SpyterDev <151678966+SpyterDev@users.noreply.github.com>
2024-11-09 20:14:24 -0500

    Update Raycast.h

--- Raycast.h
+++ Raycast.h
@@ -1,7 +1,7 @@
 /*
     MIT License
 
-    Copyright (c) 2024 Nicholas A. Fraidakis
+    Copyright (c) 2024 SpyterDev
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
